e05ef226fe70063f724a53bbf21cd73a73a70655
IgorPototskyi <igorekpototsky@gmail.com>
2017-06-11 21:49:17 +0300

    add footer, adaptive view

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,15 @@
 $(function() {
 
-	// Custom JS
+// ----- Top button
+	$(window).scroll(function() {
+		if ($(this).scrollTop() > $(this).height()) {
+			$('.top').addClass('active');
+		} else {
+			$('.top').removeClass('active');
+		}
+	});
+	$('.top').click(function() {
+		$('html, body').stop().animate({scrollTop: 0}, 'slow', 'swing');
+	});
 
 });
