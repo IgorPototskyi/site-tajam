dd05f10462d4539b00600c698b71274248104486
IgorPototskyi <igorekpototsky@gmail.com>
2017-06-19 16:36:19 +0300

    add light gallery

--- app/js/common.js
+++ app/js/common.js
@@ -12,4 +12,15 @@ $(function() {
 		$('html, body').stop().animate({scrollTop: 0}, 'slow', 'swing');
 	});
 
+	// lightGallery(document.getElementById('aniimated-thumbnials'), {
+	// 	thumbnail:true,
+	// 	animateThumb: false,
+	// }); 
+	lightGallery(document.getElementById('lightgallery')); 
 });
+
+// ----- Preloader
+$(window).on('load', function() {
+	$('.preloader').delay(1000).fadeOut('slow');
+})
+
--- gulpfile.js
+++ gulpfile.js
@@ -20,7 +20,7 @@ gulp.task('common-js', function() {
 		'app/js/common.js',
 		])
 	.pipe(concat('common.min.js'))
-	.pipe(uglify())
+	// .pipe(uglify())
 	.pipe(gulp.dest('app/js'));
 });
 
@@ -28,6 +28,10 @@ gulp.task('js', ['common-js'], function() {
 	return gulp.src([
 		'app/libs/jquery/dist/jquery.min.js',
 		'app/libs/bootstrap/js/bootstrap.min.js',
+		'app/libs/lightgallery/js/lightgallery.min.js',
+		// 'app/libs/lightgallery/js/lg-thumbnail.min.js',
+		// 'app/libs/lightgallery/js/lg-fullscreen.min.js',
+
 		'app/js/common.min.js', // Всегда в конце
 		])
 	.pipe(concat('scripts.min.js'))
